4252cb923983f62346c3728c4b48975d5b094745
CBX <chenbingxu369@gmail.com>
2025-05-29 11:05:30 -0500

    fix some bugs on ROCm

--- python/sglang/srt/layers/moe/ep_moe/kernels.py
+++ python/sglang/srt/layers/moe/ep_moe/kernels.py
@@ -561,6 +561,7 @@ def grouped_gemm_triton_kernel(
     BLOCK_SIZE_M: tl.constexpr,
     BLOCK_SIZE_N: tl.constexpr,
     BLOCK_SIZE_K: tl.constexpr,
+    USE_BF16_DOT: tl.constexpr,
 ):
     c_dtype = c.dtype.element_ty
 
@@ -614,7 +615,18 @@ def grouped_gemm_triton_kernel(
             offs_ks = k_start // group_k
             a_scale = tl.load(a_scale_ptrs + offs_ks * as_stride_1)
             b_scale = tl.load(b_scale_ptrs + offs_ks * bs_stride_2)
-            accumulator += tl.dot(a_tile, b_tile.T) * a_scale * b_scale[None, :]
+            
+            if USE_BF16_DOT:
+                # MI300X: up-cast to BF16 first
+                a_tile_fp16 = a_tile.to(tl.bfloat16)
+                b_tile_fp16 = b_tile.to(tl.bfloat16)
+                dot_res = tl.dot(a_tile_fp16, b_tile_fp16.T)
+            else:
+                # Original path (may trigger f8→f16 cast)
+                dot_res = tl.dot(a_tile, b_tile.T)
+
+            accumulator += dot_res * a_scale * b_scale[None, :]
+
         else:
             accumulator = tl.dot(a_tile, b_tile.T, accumulator)
         a_ptr += BLOCK_SIZE_K
@@ -697,6 +709,8 @@ def grouped_gemm_triton(
         triton.cdiv(a.size(0), META["BLOCK_SIZE_M"]) + batch_size,
         triton.cdiv(b.size(1), META["BLOCK_SIZE_N"]),
     )
+    
+    is_rocm = torch.version.hip
 
     grouped_gemm_triton_kernel[grid](
         a,
@@ -722,6 +736,7 @@ def grouped_gemm_triton(
         scale_b.stride(2) if scale_b is not None and scale_b.ndim == 3 else 0,
         scale_b.stride(1) if scale_b is not None and scale_b.ndim >= 2 else 0,
         **config,
+        USE_BF16_DOT=int(is_rocm is not None),
     )
     return c
 
