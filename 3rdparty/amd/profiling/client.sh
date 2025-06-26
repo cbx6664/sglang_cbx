0b703cabddb501ab62c46a0a01afeb06c4ff9ecc
CBX <chenbingxu369@gmail.com>
2025-06-25 23:18:29 -0700

    [profile] output few token for profiling client.sh command

--- 3rdparty/amd/profiling/client.sh
+++ 3rdparty/amd/profiling/client.sh
@@ -11,11 +11,10 @@ LOGFILE="sglang_client_log_$TIMESTAMP.json"
 # Run the benchmark with specified parameters and save logs
 python3 -m sglang.bench_serving \
     --backend sglang \
-    --tokenizer /home/bingxche/deepseek-v3-5layer \
-    --dataset-name random \
-    --random-input-len 100\
-    --random-output-len 2 \
-    --num-prompts 5 \
+    --model /home/bingxche/deepseek-v3-5layer \
+    --dataset-name sharegpt \
+    --sharegpt-output-len 10 \
+    --num-prompts 1 \
     --output-file online.jsonl 2>&1 | tee "$LOGFILE"
 
 # Stop profiling via API
@@ -23,4 +22,4 @@ curl http://localhost:30000/stop_profile -H "Content-Type: application/json"
 
 # Convert tracing file to csv & json
 sqlite3 trace.rpd ".mode csv" ".header on" ".output trace.csv" "select * from top;" ".output stdout"
-python3 /sgl-workspace/rocmProfileData/tools/rpd2tracing.py trace.rpd trace.json
+python3 /home/bingxche/bind/sglang_cbx/3rdparty/amd/profiling/rocmProfileData/tools/rpd2tracing.py trace.rpd trace.json
